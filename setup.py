1a11955eec8256a42dad90155bb910e220939f57
D Prakash <54624250+DPrakashMewari@users.noreply.github.com>
2021-03-31 14:58:30 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,17 @@
+# replace with your username:
+name = "Mathsimple"
+version = 0.0.1
+author = "Chandra Prakash Mewari"
+author_email = "Prakash.mewari@yahoo.com"
+description = "A small example package"
+long_description = file: README.md
+long_description_content_type = text/markdown
+url = "https://github.com/DPrakashMewari/Mathsimple"
+classifiers =
+    Programming Language :: Python :: 3
+    License :: OSI Approved :: MIT License
+    Operating System :: OS Independent
+
+
+python_requires = >=3.6
+
